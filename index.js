48f1120c0f4f55cbb9662aa2816ab1ac2f37573e
alcously <162753749+alcously@users.noreply.github.com>
2024-03-08 22:38:12 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["I won't regret the way I lived"];
 
 
 let currentIndex = 0;
